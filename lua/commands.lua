a03fb48c5f4e04dc49ffb89fab86c4ec6cb936d8
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-10 09:03:40 +0200

    logs

--- lua/commands.lua
+++ lua/commands.lua
@@ -45,7 +45,9 @@ M.choose_and_run_command = function(commands)
 
 		if selected_cmd and type(selected_cmd) == "function" then
 			local command_description = selected_cmd(name, buf)
+			vim.notify("determining command type", vim.log.levels.INFO)
 			local command_type = command_description.type or "terminal"
+			vim.notify("command type: " .. command_type, vim.log.levels.INFO)
 
 			if command_type == "terminal" then
 				M.open_terminal_and_run_command(command_description)
