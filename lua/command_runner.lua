bdb6d9dcf7aea5b63aa43523eff5ee3ea477649f
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-10 20:22:56 +0200

    log level DEBUG

--- lua/command_runner.lua
+++ lua/command_runner.lua
@@ -9,9 +9,9 @@ local function register(ext, command_list)
 		M._commands[ext] = {}
 	end
 
-	vim.notify("registering commands for " .. ext, vim.log.levels.INFO)
+	vim.notify("registering commands for " .. ext, vim.log.levels.DEBUG)
 	for _, cmd in ipairs(command_list) do
-		vim.notify("registering command " .. cmd.label, vim.log.levels.INFO)
+		vim.notify("registering command " .. cmd.label, vim.log.levels.DEBUG)
 		table.insert(M._commands[ext], cmd)
 	end
 end
@@ -19,27 +19,27 @@ end
 ---@param opts table<BuiltinCommands, BuiltinCommandOpts>
 local function register_builtin_commands(opts)
 	if opts.ts_vitest == nil or opts.ts_vitest.enable then
-		vim.notify("enable ts_vitest", vim.log.levels.INFO)
+		vim.notify("enable ts_vitest", vim.log.levels.DEBUG)
 		register("ts", require("builtin.vitest").commands)
 	end
 
 	if opts.ts_playwright == nil or opts.ts_playwright.enable then
-		vim.notify("enable ts_playwright", vim.log.levels.INFO)
+		vim.notify("enable ts_playwright", vim.log.levels.DEBUG)
 		register("ts", require("builtin.playwright").commands)
 	end
 
 	if opts.cs_dotnet_test == nil or opts.cs_dotnet_test.enable then
-		vim.notify("enable cs_dotnet_test", vim.log.levels.INFO)
+		vim.notify("enable cs_dotnet_test", vim.log.levels.DEBUG)
 		register("cs", require("builtin.dotnet_test").commands)
 	end
 
 	if opts.lua_plenary == nil or opts.lua_plenary.enable then
-		vim.notify("enable lua_plenary", vim.log.levels.INFO)
+		vim.notify("enable lua_plenary", vim.log.levels.DEBUG)
 		register("lua", require("builtin.lua_plenary").commands)
 	end
 
 	if opts.elixir_mix == nil or opts.elixir_mix.enable then
-		vim.notify("enable elixir_mix", vim.log.levels.INFO)
+		vim.notify("enable elixir_mix", vim.log.levels.DEBUG)
 		local elixir = require("builtin.elixir_mix")
 		register(":directory", elixir.directory_commands)
 		register("ex", elixir.commands)
@@ -47,7 +47,7 @@ local function register_builtin_commands(opts)
 	end
 
 	if opts.elixir_phoenix == nil or opts.elixir_phoenix.enable then
-		vim.notify("enable elixir_phoenix", vim.log.levels.INFO)
+		vim.notify("enable elixir_phoenix", vim.log.levels.DEBUG)
 		local elixir_phoenix = require("builtin.elixir_phoenix")
 		register(":directory", elixir_phoenix.directory_commands)
 		register("ex", elixir_phoenix.commands)
@@ -70,12 +70,12 @@ M.setup = function(opts)
 	local project_local_nvim_config = vim.fs.root(0, ".nvim")
 
 	if project_local_nvim_config then
-		vim.notify("Found project level nvim config", vim.log.levels.INFO)
+		vim.notify("Found project level nvim config", vim.log.levels.DEBUG)
 		local project_local_commands_file = project_local_nvim_config .. "/.nvim/command_runner.lua"
-		vim.notify("Searching " .. project_local_commands_file, vim.log.levels.INFO)
+		vim.notify("Searching " .. project_local_commands_file, vim.log.levels.DEBUG)
 
 		if vim.fn.filereadable(project_local_commands_file) == 1 then
-			vim.notify("Found project level command config", vim.log.levels.INFO)
+			vim.notify("Found project level command config", vim.log.levels.DEBUG)
 			local chunk, err = loadfile(project_local_commands_file)
 			if chunk then
 				local project_local_commands = chunk()
